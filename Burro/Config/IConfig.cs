5a13914e807f31fabcaa5505366bb56a8bb71bdb
Nathan Jones <thenathanjones@gmail.com>
2011-11-05 15:35:52 +0800

    Updated the config and version numbers to reflect the changes made to Luces

--- Burro/Config/IConfig.cs
+++ Burro/Config/IConfig.cs
@@ -5,8 +5,11 @@ namespace Burro.Config
     public interface IConfig
     {
         string URL { get; set; }
+
         string Username { get; set; }
+
         string Password { get; set; }
+
         IEnumerable<string> Pipelines { get; set; }
     }
 }
--- Burro/Properties/AssemblyInfo.cs
+++ Burro/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Burro")]
 [assembly: AssemblyDescription("Build pipeline parser, named for its grunt like ability to parse build output and dump it out as something meaningful")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Nathan Jones")]
+[assembly: AssemblyCompany("thenathanjones")]
 [assembly: AssemblyProduct("Burro")]
-[assembly: AssemblyCopyright("Copyright © 2011")]
+[assembly: AssemblyCopyright("Copyright (c) 2011 Nathan Jones (https://github.com/thenathanjones)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.10.0.0")]
+[assembly: AssemblyFileVersion("0.10.0.0")]
