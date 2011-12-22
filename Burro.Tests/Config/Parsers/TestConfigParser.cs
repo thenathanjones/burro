f15875bd317ba2f25979f188e8964f10a51406fe
unknown <Nathan Jones@.(none)>
2011-12-22 10:41:38 +0800

    Fixed the tests up around the core by wiring up the TestConfigParser which gets the mock

---
+++ Burro.Tests/Config/Parsers/TestConfigParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Burro.BuildServers;
+using Moq;
+using Ninject;
+using YamlDotNet.RepresentationModel;
+
+// ReSharper disable CheckNamespace
+// Required for dynamic loading of class
+namespace Burro.Config.Parsers
+// ReSharper restore CheckNamespace
+{
+    public class TestConfigParser : IConfigParser
+    {
+        private IKernel _kernel;
+
+        public TestConfigParser(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public IBuildServer Parse(YamlMappingNode config)
+        {
+            return _kernel.Get<IBuildServer>("TestBuildServer");
+        }
+    }
+}
